14dbd7230e55ac6ff0fb1be84bcc21d7cfb09ce3
eccentricality <youngmin13@gmail.com>
2021-10-08 10:53:55 -0400

    typeDefs established

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,44 @@
+// graphql dependency
+const { gql } = require("apollo-server-express");
+
+const typeDefs = gql`
+  type User {
+    _id: ID!
+    username: String!
+    email: String!
+    password: String!
+    bookCount: Int
+    savedBooks: [Book]!
+  }
+  type Book {
+    bookId: String
+    authors: [String]
+    description: String
+    title: String
+    image: String
+    link: String
+  }
+  type Auth {
+    token: ID!
+    user: User
+  }
+  type Query {
+    me: User
+  }
+  type Mutation {
+    addUser(username: String!, email: String!, password: String!): Auth
+    login(email: String!, password: String!): Auth
+    saveBook(
+      bookId: String!
+      authors: [String]
+      description: String
+      image: String
+      link: String
+      title: String!
+    ): User
+    removeBook(bookId: String!): User
+  }
+`;
+
+// export
+module.exports = typeDefs;

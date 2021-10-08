f3300ddd9e08894b18fa8f713fce5ce2599cddc9
eccentricality <youngmin13@gmail.com>
2021-10-08 11:10:45 -0400

    mutations util

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,79 @@
+import { gql } from "@apollo/client";
+
+// add user to db
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+// log in user
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+// save book to user favorites
+export const SAVE_BOOK = gql`
+  mutation saveBook(
+    $bookId: String!
+    $authors: [String]
+    $description: String
+    $image: String
+    $link: String
+    $title: String!
+  ) {
+    saveBook(
+      bookId: $bookId
+      authors: $authors
+      description: $description
+      image: $image
+      link: $link
+      title: $title
+    ) {
+      _id
+      username
+      email
+      savedBooks {
+        bookId
+        authors
+        image
+        description
+        title
+        link
+      }
+    }
+  }
+`;
+
+// remove from user favorites
+export const REMOVE_BOOK = gql`
+  mutation removeBook($bookId: String!) {
+    removeBook(bookId: $bookId) {
+      _id
+      username
+      email
+      savedBooks {
+        bookId
+        authors
+        image
+        description
+        title
+        link
+      }
+    }
+  }
+`;
